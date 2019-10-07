3b4723d737169b768b035de58ec0d2dae42d445c
Kasper Meyer <hi@kaspermeyer.com>
2019-10-07 13:30:05 +0200

    Initialize repository

---
+++ lib/interactor/async.rb
@@ -0,0 +1,8 @@
+require "interactor/async/version"
+
+module Interactor
+  module Async
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/interactor/async/version.rb
@@ -0,0 +1,5 @@
+module Interactor
+  module Async
+    VERSION = "0.1.0"
+  end
+end
