5aa16f2a57ef2b41b06b21e5f306ec3dece2ae7f
Kasper Meyer <hi@kaspermeyer.com>
2019-10-07 18:42:01 +0200

    Use Minitest Reporters for test output formatting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "interactor/async"
 
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require "minitest/autorun"
